dc0f32eeeac13e007681a0f8db6545ae648eee26
Naibo <naibowang@foxmail.com>
2021-12-10 19:43:38 +0800

    new uwsgi configuration and flask production

--- backEnd/netron/netron_flask/netron_flask.py
+++ backEnd/netron/netron_flask/netron_flask.py
@@ -16,8 +16,27 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 browsers = []
-app = Flask(__name__)
 n = 3  # 浏览器数量，即最多可同时处理的文件数量，用以多并发
+
+
+path = os.path.abspath(os.getcwd() + '/../../static/pics')
+chrome_options = webdriver.ChromeOptions()
+prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': path}
+chrome_options.add_experimental_option('prefs', prefs)
+# chrome_options.add_argument('--no-sandbox')#解决DevToolsActivePort文件不存在的报错
+chrome_options.add_argument('window-size=1920x3000')  # 指定浏览器分辨率
+chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
+chrome_options.add_argument('--disable-dev-shm-usage')
+# Solve  the problem of cannot run google-chrome in command line and must include headless to run
+# https://stackoverflow.com/questions/60304251/unable-to-open-x-display-when-trying-to-run-google-chrome-on-centos-rhel-7-5
+chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+chrome_options.binary_location = r"chrome/chrome"  # 手动指定使用的浏览器位置
+# 设置最多同时处理的任务长度，即浏览器打开的数量
+for i in range(n):
+    browser = webdriver.Chrome('./chromedriver', options=chrome_options)
+    browsers.append({"browser": browser, "status": "waiting"})
+app = Flask(__name__)
 
 
 class Device:
@@ -91,21 +110,22 @@ def test():
 
 
 if __name__ == '__main__':
-    path = os.path.abspath(os.getcwd() + '/../../static/pics')
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': path}
-    chrome_options.add_experimental_option('prefs', prefs)
-    # chrome_options.add_argument('--no-sandbox')#解决DevToolsActivePort文件不存在的报错
-    chrome_options.add_argument('window-size=1920x3000')  # 指定浏览器分辨率
-    chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
-    chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    # Solve  the problem of cannot run google-chrome in command line and must include headless to run
-    # https://stackoverflow.com/questions/60304251/unable-to-open-x-display-when-trying-to-run-google-chrome-on-centos-rhel-7-5
-    chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
-    chrome_options.binary_location = r"/usr/bin/google-chrome-stable"  # 手动指定使用的浏览器位置
-    # 设置最多同时处理的任务长度，即浏览器打开的数量
-    for i in range(n):
-        browser = webdriver.Chrome('./chromedriver', options=chrome_options)
-        browsers.append({"browser": browser, "status": "waiting"})
+    # path = os.path.abspath(os.getcwd() + '/../../static/pics')
+    # chrome_options = webdriver.ChromeOptions()
+    # prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': path}
+    # chrome_options.add_experimental_option('prefs', prefs)
+    # # chrome_options.add_argument('--no-sandbox')#解决DevToolsActivePort文件不存在的报错
+    # chrome_options.add_argument('window-size=1920x3000')  # 指定浏览器分辨率
+    # chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+    # chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
+    # chrome_options.add_argument('--disable-dev-shm-usage')
+    # # Solve  the problem of cannot run google-chrome in command line and must include headless to run
+    # # https://stackoverflow.com/questions/60304251/unable-to-open-x-display-when-trying-to-run-google-chrome-on-centos-rhel-7-5
+    # chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+    # chrome_options.binary_location = r"chrome/chrome"  # 手动指定使用的浏览器位置
+    # # 设置最多同时处理的任务长度，即浏览器打开的数量
+    # for i in range(n):
+    #     browser = webdriver.Chrome('./chromedriver', options=chrome_options)
+    #     browsers.append({"browser": browser, "status": "waiting"})
+    # 注意，在uwsgi配置的生产环境中，前面这堆代码是不会执行的，所以要配置到外面公共区域
     app.run('0.0.0.0', 8078, True, processes=True)
